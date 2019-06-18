7f629ab8cddacf429428dd92cc0351b802d0631e
likebeta <ixxoo.me@gmail.com>
2019-06-18 20:58:38 +0800

    update

--- footer.php
+++ footer.php
@@ -1,7 +1,5 @@
 <div id="footer">
 	<div id="copyright" class="container">
-		<p class="muted credit">
-			Copyright &copy; <a href="https://github.com/messycode" target="_blank">messycode</a> All Rights Reserved
-		</p>
+		<p class="muted credit">Copyright &copy; <a href="https://github.com/messycode" target="_blank">messycode</a>, All Rights Reserved,Power by <a href="https://twitter.github.io/bootstrap/" target="_blank">bootstrap</a>,Design by <a href="https://www.ixxoo.me" target="_blank">likebeta</a></p>
 	</div>
 </div>
