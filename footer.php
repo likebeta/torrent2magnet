dca183db590a29a8bd477ec35584415b1f428169
likebeta <ixxoo.me@gmail.com>
2020-04-05 00:50:05 +0800

    qq stat

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <div id="footer">
+	<script type="text/javascript" src="//tajs.qq.com/stats?sId=66540845" charset="utf-8"></script>
 	<div id="copyright" class="container">
 		<p class="muted credit">Copyright &copy; <a href="https://github.com/messycode" target="_blank">messycode</a>, All Rights Reserved,Power by <a href="https://twitter.github.io/bootstrap/" target="_blank">bootstrap</a>,Design by <a href="https://www.ixxoo.me" target="_blank">likebeta</a></p>
 	</div>
